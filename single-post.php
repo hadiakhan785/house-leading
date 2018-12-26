785b032d84525a83423310342bd60e644dd966e8
hadiakhan785 <hadiakhan785@gmail.com>
2018-12-26 16:28:13 +0500

    single post updated

--- single-post.php
+++ single-post.php
@@ -21,45 +21,64 @@
       <div class="single-post card">
                 <div class="card-head">
                   <img src="images/feature1.jpg" alt="feature image">
-                  <div class="cost">
-                    <p class="cost-btn">
-                      $954 890
-                    </p>
-                  </div>
                 </div>
 
                 <div class="card-body">
-                  <h5>Vital Villas</h5>
+                  <h5 class="post-title">Vital Villas</h5>
                   <p class="location">
                     <i class="icon-hl-gps"></i> Upper Road 3411, no.34 CA
                   </p>
 
-                  <p class="description">
-                  Integer nec bibendum lacus. Suspendisse dictum enim sit amet libero malesuada.
-                  <a href="single-post.php" class="more-link">Read More</a>
+                  <p class="description post-details"> 
+                    Integer nec bibendum lacus. Suspendisse dictum enim sit amet libero malesuada.Integer nec bibendum lacus. Suspendisse dictum enim sit amet libero malesuada.Integer nec bibendum lacus. Suspendisse dictum enim sit amet libero malesuada.Integer nec bibendum lacus. Suspendisse dictum enim sit amet libero malesuada.Integer nec bibendum lacus. Suspendisse dictum enim sit amet libero malesuada.Integer nec bibendum lacus. Suspendisse dictum enim sit amet libero malesuada.Integer nec bibendum lacus. Suspendisse dictum enim sit amet libero malesuada.Integer nec bibendum lacus. Suspendisse dictum enim sit amet libero malesuada.Integer nec bibendum lacus. Suspendisse dictum enim sit amet libero malesuada.Integer nec bibendum lacus. Suspendisse dictum enim sit amet libero malesuada.Integer nec bibendum lacus. Suspendisse dictum enim sit amet libero malesuada.Integer nec bibendum lacus. Suspendisse dictum enim sit amet libero malesuada.Integer nec bibendum lacus. Suspendisse dictum enim sit amet libero malesuada.Integer nec bibendum lacus. Suspendisse dictum enim sit amet libero malesuada.  
                   </p>
+                  <div class="further-detail flex-d">
+                  <div class="bathroom post-details">
+                    <p>
+                      <i class="icon-hl-bathroom"></i><br>
+                      <span class="bathroom-quantity">2</span><br>
+                      Bathroom
+                    </p>                    
+                  </div>
 
-                  <div class="property-meta-data flex-d">
-                    <div class="new-tag flex-d">
-                      <i class="icon-hl-new"></i>
-                    </div>
+                  <div class="bedroom post-details">
+                    <p>
+                      <i class="icon-hl-bed"></i><br>
+                      <span class="rooms-quantity">2</span><br>
+                      Bedroom 
+                    </p>
+                   
+                  </div>
+                  <div class="space post-details">
+                    <p>
+                      <i class="icon-hl-resize"></i><br>
+                      <span class="space-measurement">120 sq ft</span><br>
+                      Area
+                    </p>       
+                  </div>
 
-                    <div class="bathroom flex-d">
-                      <i class="icon-hl-bathroom"></i>
-                      <span class="bathroom-quantity">2</span>
-                    </div>
-                    <div class="bedroom flex-d">
-                      <i class="icon-hl-bed"></i>
-                      <span class="rooms-quantity">2</span>
-                    </div>
-                    <div class="space flex-d">
-                      <i class="icon-hl-resize"></i>
-                      <span class="space-measurement">120 sq ft</span>
-                      
-                    </div>
-                  </div>  
-                </div>
+                  <div class="category post-details">
+                    <p>
+                      <span class="category">Home</span><br>
+                      Category
+                    </p>       
+                  </div>
+
+                  <div class="post-details">
+                    <p>
+                      <span class="product-price">$954 890</span><br>
+                      price
+                    </p>       
+                  </div>
+                  </div>
+                  
+                    <button class="btn-secndry">
+                      BOOK NOW
+                    </button>
+                  
+                </div>  
               </div>
+            </div>
              <!-- Card 1 End -->
     </div>
   </div>
